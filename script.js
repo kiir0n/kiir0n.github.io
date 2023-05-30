773c8dd0d9577de557d622d01884499304335672
kiir0n <88636812+kiir0n@users.noreply.github.com>
2023-05-30 21:46:21 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,41 @@
-function toggleLightMode() {
-    const body = document.querySelector('body');
-    body.classList.toggle('light-mode');
-  }
+
+let knowmore = document.getElementById('knowmore');
+
+knowmore.addEventListener('click', function() {
+  window.location.href = 'knowmore.html';
+});
+
+(function() {
+
+    var quotes = $(".quotes1");
+    var quoteIndex = -1;
   
+    function showNextQuote() {
+      ++quoteIndex;
+      quotes.eq(quoteIndex % quotes.length)
+        .fadeIn(1000)
+        .delay(1000)
+        .fadeOut(1000, showNextQuote);
+    }
+  
+    showNextQuote();
+  
+  })();
+
+
+(function() {
+
+    var quotes = $(".quotes");
+    var quoteIndex = -1;
+  
+    function showNextQuote() {
+      ++quoteIndex;
+      quotes.eq(quoteIndex % quotes.length)
+        .fadeIn(1000)
+        .delay(1000)
+        .fadeOut(1000, showNextQuote);
+    }
+  
+    showNextQuote();
+  
+  })();
